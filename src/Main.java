a5a18e0de981e505d8ff2ce4c30a2589220f8ba1
Frank Hedecke <frank@ifsr.de>
2015-01-22 20:04:12 +0100

    testing serial concatenation

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,52 @@ public class Main {
 
 		ErrorSource error = new ErrorSource(25, 1337l);
 
-		Channel encodeChannel = code.getEncodeChannel();
+		Channel encode1Channel = code.getEncodeChannel();
+		Channel encode2Channel = code.getEncodeChannel();
 		Channel noisyChannel = new NoisyChannel(error);
-		Channel decodeChannel = code.getDecodeChannel();
+		Channel decode1Channel = code.getDecodeChannel();
+		Channel decode2Channel = code.getDecodeChannel();
 
-		System.out.println("input data");
-
+		System.out.print("input data :     ");
 		for (int i = 0; i < 40; i++) {
 			if (random.hasOutput()) {
 				int bit = random.getOutput();
 				System.out.print(bit);
-				encodeChannel.pushInput(bit);
+				encode1Channel.pushInput(bit);
 			}
 		}
 
-		System.out.println("\n\nencoded data");
+		System.out.print("\n\nencoded data 1 : ");
+		while (encode1Channel.hasOutput()) {
+			int bit = encode1Channel.getOutput();
+			System.out.print(bit);
+			encode2Channel.pushInput(bit);
+		}
 
-		while (encodeChannel.hasOutput()) {
-			int bit = encodeChannel.getOutput();
+		System.out.print("\n\nencoded data 2 : ");
+		while (encode2Channel.hasOutput()) {
+			int bit = encode2Channel.getOutput();
 			System.out.print(bit);
 			noisyChannel.pushInput(bit);
 		}
 
-		System.out.println("\n\nencoded data with noise");
-
+		System.out.print("\n\nwith noise :     ");
 		while (noisyChannel.hasOutput()) {
 			int bit = noisyChannel.getOutput();
 			System.out.print(bit);
-			decodeChannel.pushInput(bit);
+			decode1Channel.pushInput(bit);
 		}
 
-		System.out.println("\n\ndecoded data");
+		System.out.print("\n\ndecoded data 1 : ");
+		while (decode1Channel.hasOutput()) {
+			int bit = decode1Channel.getOutput();
+			System.out.print(bit);
+			decode2Channel.pushInput(bit);
+		}
 
-		while (decodeChannel.hasOutput()) {
-			int bit = decodeChannel.getOutput();
+		System.out.print("\n\ndecoded data 2 : ");
+		while (decode2Channel.hasOutput()) {
+			int bit = decode2Channel.getOutput();
 			System.out.print(bit);
 		}
 	}
