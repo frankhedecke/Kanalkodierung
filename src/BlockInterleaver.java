b0e9f6c9d5243879f7f5d077403b971f9b9266f1
Frank Hedecke <frank@ifsr.de>
2015-01-26 22:44:07 +0100

    BlockInterleaver

---
+++ src/BlockInterleaver.java
@@ -0,0 +1,34 @@
+public class BlockInterleaver extends AbstractBufferChannel {
+
+	private int[][] inputBuffer;
+	private int ptrX;
+	private int ptrY;
+	private int depth;
+
+	public BlockInterleaver(int depth) {
+		this.inputBuffer = new int[depth][depth];
+		this.depth = depth;
+		this.ptrX = 0;
+		this.ptrY = 0;
+	}
+
+	@Override
+	public void pushInput(int bit) {
+		this.inputBuffer[this.ptrX][this.ptrY] = bit;
+		this.ptrY++;
+		if (this.ptrY >= this.depth) {
+			this.ptrY = 0;
+			this.ptrX++;
+		}
+
+		if (this.ptrX >= this.depth) {
+			this.ptrX = 0;
+
+			for (int i = 0; i < this.depth; i++) {
+				for (int j = 0; j < this.depth; j++) {
+					this.buffer.add(this.inputBuffer[j][i]);
+				}
+			}
+		}
+	}
+}
