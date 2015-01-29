7b8849c8abe9e4d6ad736222890305b786ab6852
Frank Hedecke <frank@ifsr.de>
2015-01-29 11:27:48 +0100

    fix - forgot IntegerBufferChannel

---
+++ src/IntegerBufferChannel.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class IntegerBufferChannel implements Channel<Integer> {
+
+	protected List<Integer> buffer;
+
+	public IntegerBufferChannel() {
+		this.buffer = new LinkedList<Integer>();
+	}
+
+	@Override
+	public boolean hasOutput() {
+		return !buffer.isEmpty();
+	}
+
+	@Override
+	public Integer getOutput() {
+		Integer bit = buffer.remove(0);
+		return bit;
+	}
+}
