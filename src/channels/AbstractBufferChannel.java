2fe65efb4417d3cf06a146f3a5305d6e6e7462a1
Frank Hedecke <frank@ifsr.de>
2015-07-10 18:53:36 +0200

    refactor buffering channels

---
+++ src/channels/AbstractBufferChannel.java
@@ -0,0 +1,24 @@
+package channels;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class AbstractBufferChannel<T> implements Channel<T> {
+	protected List<T> buffer;
+
+	public AbstractBufferChannel() {
+		this.buffer = new LinkedList<T>();
+	}
+
+	@Override
+	public boolean hasOutput() {
+		return !buffer.isEmpty();
+	}
+
+	@Override
+	public T getOutput() {
+		T bit = buffer.remove(0);
+		return bit;
+	}
+
+}
--- src/channels/IntegerBufferChannel.java
+++ src/channels/IntegerBufferChannel.java
@@ -1,23 +1,4 @@
 package channels;
-import java.util.LinkedList;
-import java.util.List;
 
-public abstract class IntegerBufferChannel implements Channel<Integer> {
-
-	protected List<Integer> buffer;
-
-	public IntegerBufferChannel() {
-		this.buffer = new LinkedList<Integer>();
-	}
-
-	@Override
-	public boolean hasOutput() {
-		return !buffer.isEmpty();
-	}
-
-	@Override
-	public Integer getOutput() {
-		Integer bit = buffer.remove(0);
-		return bit;
-	}
+public abstract class IntegerBufferChannel extends AbstractBufferChannel<Integer> {
 }
