3e30d347e4dfc5fbf68fd75a71e420867c145568
Frank Hedecke <frank@ifsr.de>
2017-01-05 19:05:03 +0100

    AWGN noise channel

--- src/Tests/TestPCHammingCodeAWGN.java
+++ src/Tests/TestPCHammingCodeAWGN.java
@@ -5,7 +5,6 @@ import blockcode.HammingCode_4a;
 import channels.Channel;
 import channels.ErrorSourceAWGN;
 import channels.NoisyChannelAWGN;
-import channels.PC_HardIterDecodeChannel;
 import channels.RandomSource;
 import main.Modulator;
 import main.ParallelConcatenation;
@@ -20,9 +19,14 @@ public class TestPCHammingCodeAWGN {
 		Channel<Integer> src = new RandomSource(2, 823649l);
 		Channel<Integer> encoder = pcat.getEncodeChannel();
 		Channel<Float> decoder = pcat.getSoftDecodeChannel();
-		
-		ErrorSourceAWGN errorSource  = new ErrorSourceAWGN(0f, 0f);
+
+		float codeRate = 0.5f;
+		float snr_dB = 4.0f;
+
+		ErrorSourceAWGN errorSource = new ErrorSourceAWGN(codeRate, snr_dB, 1337);
 		Channel<Float> noisyChannel = new NoisyChannelAWGN(errorSource);
+
+		System.out.println("RauschVarianz = " + errorSource.getVariance());
 
 		System.out.print("  input = ");
 		for (int i = 0; i < 16; i++) {
@@ -31,19 +35,21 @@ public class TestPCHammingCodeAWGN {
 			encoder.pushInput(bit);
 		}
 		
-		System.out.print("\nwithout noise = ");
+		System.out.print("\n without noise = ");
 		while(encoder.hasOutput()) {
 			float bit = Modulator.hardToSoft(encoder.getOutput());
-			System.out.print(bit + " ");
+			System.out.format("%2.3f ", bit);
 			noisyChannel.pushInput(bit);
 		}
 		
-		System.out.print("\nwith noise    = ");
+		System.out.print("\n with noise    = ");
 		while(noisyChannel.hasOutput()) {
 			float bit = noisyChannel.getOutput();
-			System.out.print(bit + " ");
+			System.out.format("%2.3f ", bit);
 			decoder.pushInput(bit);
 		}
+
+		//TODO Output or output order is somehow broken
 
 		System.out.print("\n output = ");
 		while(decoder.hasOutput()) {
--- src/channels/ErrorSourceAWGN.java
+++ src/channels/ErrorSourceAWGN.java
@@ -1,21 +1,32 @@
 package channels;
+import java.util.Random;
 
 public class ErrorSourceAWGN extends AbstractSource<Float> {
 	
-	private float coteRate;
-	private float noise_dB;
+	private float codeRate;
+	private float snr_dB;
+	private Random random;
 	
-	public ErrorSourceAWGN(float codeRate, float noise_dB) {
+	public ErrorSourceAWGN(float codeRate, float snr_dB, long seed) {
 		
 		// TODO check codeRate
 		// TODO check noise_dB
-		this.coteRate = codeRate;
-		this.noise_dB = noise_dB;
+		this.codeRate = codeRate;
+		this.snr_dB = snr_dB;
+		this.random = new Random(seed);
 	}
 
 	@Override
 	public Float getOutput() {
-		// TODO DELETE, just a stub for testing
-		return 0.2f;
+		// rand_val is gaussian distributed with a standard derivation = 1.0 (and mean = 0.0)
+		double random_value = this.random.nextGaussian();
+		// adjust the standard derivation
+		double error_value = random_value * Math.sqrt(this.getVariance());
+
+		return (float) error_value;
+	}
+
+	public Double getVariance() {
+		return (1.0 / (2.0 * codeRate * Math.pow(10, snr_dB / 10)));
 	}
 }
