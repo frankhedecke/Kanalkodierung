d8bdacd8574cd1c0b0bafd5e848ddd930f1ba169
Frank Hedecke <frank@ifsr.de>
2017-03-03 16:39:45 +0100

    new IntegerInputChannel

--- src/channels/DecodeChannel.java
+++ src/channels/DecodeChannel.java
@@ -3,32 +3,22 @@ package channels;
 import entity.BinaryWord;
 import blockcode.BlockCode;
 
-public class DecodeChannel extends IntegerBufferChannel {
+public class DecodeChannel extends IntegerInputChannel {
 
 	private BlockCode code;
-	private int[] inputBuffer;
-	private int inputPtr;
 
 	public DecodeChannel(BlockCode code) {
-		super();
+		super(code.getN());
 		this.code = code;
-		this.inputBuffer = new int[code.getN()];
-		this.inputPtr = 0;
 	}
 
 	@Override
-	public void pushInput(Integer bit) {
-		this.inputBuffer[this.inputPtr] = bit;
-		this.inputPtr++;
+	public void process() {
+		BinaryWord input = new BinaryWord(inputBuffer);
+		BinaryWord output = code.decode(input);
 
-		if (this.inputPtr == this.inputBuffer.length) {
-			this.inputPtr = 0;
-			BinaryWord input = new BinaryWord(inputBuffer);
-			BinaryWord output = code.decode(input);
-
-			for (int i : output.toArray()) {
-				super.buffer.add(i);
-			}
+		for (int i : output.toArray()) {
+			super.buffer.add(i);
 		}
 	}
 }
---
+++ src/channels/IntegerInputChannel.java
@@ -0,0 +1,26 @@
+package channels;
+
+public abstract class IntegerInputChannel extends IntegerBufferChannel {
+
+	protected int[] inputBuffer;
+	protected int inputBufferSize;
+	private int inputCursor;
+
+	IntegerInputChannel(int inputBufferSize) {
+		this.inputBufferSize = inputBufferSize;
+		this.inputCursor = 0;
+		this.inputBuffer = new int[inputBufferSize];
+	}
+
+	@Override
+	public void pushInput(Integer bit) {
+		this.inputBuffer[this.inputCursor] = bit;
+
+		if (this.inputCursor == this.inputBufferSize) {
+			this.inputCursor = 0;
+			process();
+		} 
+	}
+
+	public abstract void process();
+}
